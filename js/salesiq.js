e123b9aed6a623ad8b87179d94c8611a7a8ae811
219003113 <42488276+219003113@users.noreply.github.com>
2020-01-13 09:59:14 +0530

    Update salesiq.js

--- js/salesiq.js
+++ js/salesiq.js
@@ -1,3 +1,3 @@
-<script type="text/javascript">
+
 var $zoho=$zoho || {};$zoho.salesiq = $zoho.salesiq || {widgetcode:"462e871e254500559641417c418b3f2e096f91c4e1c774b3f205fe8f3d9eee9e", values:{},ready:function(){}};var d=document;s=d.createElement("script");s.type="text/javascript";s.id="zsiqscript";s.defer=true;s.src="https://lab1salesiq.localzoho.com/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);d.write("<div id='zsiqwidget'></div>");
-</script>
+
